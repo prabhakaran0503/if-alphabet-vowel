fa000486d488b5ba1ae73b2bda216f88affe5c9d
prabhakaran <prabhakaranplkk@gmail.com>
2023-09-05 21:27:37 +0530

    alphabet

---
+++ css/main.js
@@ -0,0 +1,11 @@
+var n=prompt("Enter the only one leter");
+
+if(n==="a"||"e"||"i"||"o"||"u"){
+    document.write("The alphabet is vowel")
+}
+else if(n==="b"||"c"||"d"||"f"||"g"||"h"||"j"||"k"||"l"||"m"||"n"||"p"||"q"||"r"||"s"||"t"||"v"||"w"||"x"||"y"||"z"){
+     document.write("The alphabet is consonant")   
+}
+else(
+    document.write("Enter the only one leter")
+)
